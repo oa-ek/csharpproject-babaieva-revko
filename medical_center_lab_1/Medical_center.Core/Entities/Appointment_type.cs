314887c8d9d3436f8a4eddef6c3f8bbcd15d3e8d
NastyaRevko <152927583+NastyaRevko@users.noreply.github.com>
2024-04-10 12:02:30 +0300

    Nastya

---
+++ medical_center_lab_1/Medical_center.Core/Entities/Appointment_type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_center.Core.Entities
+{
+	internal class Appointment_type
+	{
+		public string Title {  get; set; }
+		public Guid appTypeId { get; set; }
+	}
+}
