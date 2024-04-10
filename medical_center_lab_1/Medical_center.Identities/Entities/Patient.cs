3c3f3fa50483833728f1b57ed1ffe823d0ba0546
Viktoriia Babaieva <viktoriia.babaieva@oa.edu.ua>
2024-04-10 13:21:02 +0300

    Vika

---
+++ medical_center_lab_1/Medical_center.Identities/Entities/Patient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_center.Core.Entities
+{
+    internal class Patient
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Name { get; set; } = string.Empty;
+        public string? Surname { get; set; } = string.Empty;
+        public string? Patronimic { get; set; } = string.Empty;
+        public bool gender { get; set; }
+        public int? age { get; set; }
+        public string? p_photo { get; set; }
+        public string? adress { get; set; }
+        public DateOnly? date_of_birth { get; set; }
+        public string? phone_number {  get; set; }
+        public string? email { get; set; }
+        public virtual ICollection<Diagnose> med_record { get; set; } = new HashSet<Diagnose>();
+        public virtual ICollection<Appointment> appointments { get; set; } = new HashSet<Appointment> ();
+        public virtual ICollection<Comment> comments { get; set; } = new HashSet<Comment>();
+    }
+}
