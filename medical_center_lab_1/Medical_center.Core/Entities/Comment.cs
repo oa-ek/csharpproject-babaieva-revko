026ca608e6b0289f3daa3a39d4474020e29b73b7
NastyaRevko <152927583+NastyaRevko@users.noreply.github.com>
2024-04-10 16:29:36 +0300

    Revert "l"
    This reverts commit d069c8368e569ad52bf5f19bb6f0b3920efd74e4.

---
+++ medical_center_lab_1/Medical_center.Core/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_center.Core.Entities
+{
+	internal class Comment
+	{
+		[ForeignKey(nameof(Comment))]
+		public Guid commentId { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string MainText { get; set; } = string.Empty;
+		[ForeignKey(nameof(Doctor))]
+		public Guid doctorId { get; set; }
+		[ForeignKey(nameof(Patient))]
+		public Guid patientId { get; set; }
+
+	}
+}
