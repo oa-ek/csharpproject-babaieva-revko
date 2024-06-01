1ffa3e3db7959db58929b7ac4c60bff7379c13d4
Viktoriia Babaieva <viktoriia.babaieva@oa.edu.ua>
2024-06-02 01:55:14 +0300

    all exept user functions added

---
+++ MedicalCenter_lab1/MedicalCenter.WebUI/Controllers/CommentController.cs
@@ -0,0 +1,107 @@
+﻿using Humanizer;
+using MedicalCenter.Core.Entities;
+using MedicalCenter.Repositories.Appointments;
+using MedicalCenter.Repositories.Comments;
+using MedicalCenter.Repositories.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis;
+
+namespace MedicalCenter.WebUI.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentRepository _commentRepository;
+        private readonly IUserRepository _userRepository;
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public CommentController(
+           ICommentRepository commentRepository,
+            IUserRepository userRepository,
+            IWebHostEnvironment webHostEnvironment)
+        {
+            _commentRepository = commentRepository;
+            _userRepository = userRepository;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _commentRepository.GetAllAsync());
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Doctors = (await _userRepository.GetAllAsync())
+                .Select(x => new SelectListItem { Text = x.FullName, Value = x.Id.ToString() }).ToList();
+
+            return View(new Comment());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Comment model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _commentRepository.CreateAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Doctors = (await _userRepository.GetAllAsync())
+                .Select(x => new SelectListItem { Text = x.FullName, Value = x.Id.ToString() }).ToList();
+
+            return View(model);
+        }
+        [HttpGet("Comment/Edit/{id}")]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var comment = await _commentRepository.GetAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Doctors = (await _userRepository.GetAllAsync())
+                .Select(x => new SelectListItem { Text = x.FullName, Value = x.Id.ToString() }).ToList();
+
+            return View(comment);
+        }
+
+        [HttpPost("Comment/Edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Comment model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _commentRepository.UpdateAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Doctors = (await _userRepository.GetAllAsync())
+                .Select(x => new SelectListItem { Text = x.FullName, Value = x.Id.ToString() }).ToList();
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return View(await _commentRepository.GetAsync(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id, IFormCollection form)
+        {
+            try
+            {
+                await _commentRepository.DeleteAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction("Delete", new { id = id });
+            }
+        }
+    }
+}
---
+++ MedicalCenter_lab1/MedicalCenter.WebUI/Controllers/DiagnosisController.cs
@@ -0,0 +1,94 @@
+﻿using Humanizer;
+using MedicalCenter.Core.Entities;
+using MedicalCenter.Repositories.Diagnoses;
+using MedicalCenter.Repositories.Users;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace MedicalCenter.WebUI.Controllers
+{
+    public class DiagnosisController : Controller
+    {
+        private readonly IDiagnosisRepository _diagnosisRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public DiagnosisController(
+           IDiagnosisRepository diagnosisRepository,
+            IUserRepository userRepository,
+            IWebHostEnvironment webHostEnvironment)
+        {
+            _diagnosisRepository = diagnosisRepository;
+            _userRepository = userRepository;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var diagnoses = await _diagnosisRepository.GetAllAsync();
+            var sortedDiagnoses = diagnoses.OrderByDescending(d => d.Date).ToList();
+            return View(sortedDiagnoses);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Diagnosis());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Diagnosis model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _diagnosisRepository.CreateAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        [HttpGet("Diagnosis/Edit/{id}")]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var diagnosis = await _diagnosisRepository.GetAsync(id);
+            if (diagnosis == null)
+            {
+                return NotFound();
+            }
+            return View(diagnosis);
+        }
+
+        [HttpPost("Diagnosis/Edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Diagnosis model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _diagnosisRepository.UpdateAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return View(await _diagnosisRepository.GetAsync(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id, IFormCollection form)
+        {
+            try
+            {
+                await _diagnosisRepository.DeleteAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction("Delete", new { id = id });
+            }
+        }
+    }
+}
